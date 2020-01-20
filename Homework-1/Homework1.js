2313282acb536566db0da6c92d67743a4c63430b
StupidCodeThief <denis.vasiliuk@gmail.com>
2020-01-20 18:20:20 +0300

    Homewok 1

---
+++ Homework-1/Homework1.js
@@ -0,0 +1,62 @@
+// task 2
+
+// alert("Я JavaScript!");
+
+// task 3
+
+// const name = "John";
+// const admin = name;
+// alert(admin);
+
+// task 4
+
+// const planet ="";
+// const user ="";
+
+// task 5
+
+// let name = "Ilya";
+// alert(`hello ${1}`); // 1
+// alert(`hello ${"name"}`); // name
+// alert(`hello ${name}`); // Ilya
+
+// task 6
+
+// 10
+// -1
+// 1
+// 2
+// 6
+// 45px
+// $45
+// 2
+// NaN
+// Infinity
+//  -9 5
+// -14
+// 1
+// NaN
+// -2
+
+// task 7
+
+// let a = 1,
+//   b = 1;
+// let c = ++a; // 2, a = 2
+// let d = b++; // 1, b = 1
+// b = 2
+
+// task 8
+
+// let a = 2;
+// let x = 1 + 1 + (a *= 2); // a = 4, x = 6
+
+// task 9
+
+// true
+// false
+// true
+// true
+// false
+// false
+// false
